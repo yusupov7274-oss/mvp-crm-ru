8f165f031a1ed9292dee9198cf6e7ec3c6a85d49
yusupov7274-oss <yusupov7274@gmail.com>
2025-09-15 14:01:27 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,2 +1,9 @@
 import React from 'react';
-export default function App(){ return <div>CRM</div> }
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
