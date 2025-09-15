4ebc17801d5a5017cd8debe25a56ac2d80cbf0a0
yusupov7274-oss <yusupov7274@gmail.com>
2025-09-15 13:28:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// замените base на точное имя вашего репозитория
 export default defineConfig({
   plugins: [react()],
-  base: '/mvp-crm-ru/',
+  base: '/mvp-crm-ru/',  
 })
