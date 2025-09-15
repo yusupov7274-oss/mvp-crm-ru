9d87814a2b8f64cbda586aa354019d7ae94e0c56
yusupov7274-oss <yusupov7274@gmail.com>
2025-09-15 14:08:14 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div style={{ padding: 40, textAlign: 'center', fontFamily: 'sans-serif' }}>
       <h2 style={{ color: 'green' }}>✅ React работает</h2>
-      <p>Этот текст пришёл из <code>src/App.tsx</code>.</p>
+      <p>Этот текст из <code>src/App.tsx</code>.</p>
     </div>
   );
 }
